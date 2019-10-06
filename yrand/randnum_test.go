5518b15f26d3b178a23f0ec1fb66e89412a97171
Angel Analoosen <an9an63@gmail.com>
2019-10-06 17:29:04 +0800

    add more test cases

--- yrand/randnum_test.go
+++ yrand/randnum_test.go
@@ -16,7 +16,20 @@ func TestInt64Range(t *testing.T) {
 		wantNRange args
 		wantErr    bool
 	}{
+		{"invalid min and max", args{int64(20), int64(10)}, args{int64(0), int64(1)}, true},
 		{"same min and max", args{int64(10), int64(10)}, args{int64(0), int64(1)}, true},
+		{"always same number", args{int64(1000), int64(1001)}, args{int64(1000), int64(1001)}, false},
+		{"between [0, 100)", args{int64(0), int64(100)}, args{int64(0), int64(100)}, false},
+		{"between [0, 10000)", args{int64(0), int64(10000)}, args{int64(0), int64(10000)}, false},
+		{"between [0, 100000000)", args{int64(0), int64(100000000)}, args{int64(0), int64(100000000)}, false},
+		{"between [0, 2147483647)", args{int64(0), int64(2147483647)}, args{int64(0), int64(2147483647)}, false},
+		{"between [0, 2147483648)", args{int64(0), int64(2147483648)}, args{int64(0), int64(2147483648)}, false},
+		{"between [0, 9223372036854775807)", args{int64(0), int64(9223372036854775807)}, args{int64(0), int64(9223372036854775807)}, false},
+		{"between [-100, 0)", args{int64(-100), int64(0)}, args{int64(-100), int64(0)}, false},
+		{"between [-9223372036854775807, 0)", args{int64(-9223372036854775807), int64(0)}, args{int64(-9223372036854775807), int64(0)}, false},
+		{"between [-100, 100)", args{int64(-100), int64(100)}, args{int64(-100), int64(100)}, false},
+		{"between [-2147483648, 2147483648)", args{int64(-2147483648), int64(2147483648)}, args{int64(-2147483648), int64(2147483648)}, false},
+		// {"between [-9223372036854775808, 9223372036854775807)", args{int64(-9223372036854775808), int64(9223372036854775807)}, args{int64(-9223372036854775808), int64(9223372036854775807)}, false},
 	}
 
 	for _, tt := range tests {
