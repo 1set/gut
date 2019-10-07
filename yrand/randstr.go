baadf298c965f5017c705e2c4f83b994cca54bb7
Angel Analoosen <an9an63@gmail.com>
2019-10-07 18:05:29 +0800

    Implement basic StringBase62

--- yrand/randstr.go
+++ yrand/randstr.go
@@ -1,5 +1,49 @@
 package yrand
 
-func StringBase62(length int) (s string, err error) {
-	return "hello", nil
+import (
+	"crypto/rand"
+	"errors"
+	"math/big"
+	"strings"
+)
+
+var alphabetDigit = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var (
+	errStringLimit = errors.New("limit of string should be positive")
+)
+
+func StringBase62(limit int) (s string, err error) {
+	if limit <= 0 {
+		err = errStringLimit
+		return
+	}
+
+	base := uint64(len(alphabetDigit))
+	numBig := new(big.Int)
+	bytes := make([]byte, 8)
+
+	sb := strings.Builder{}
+	for sb.Len() < limit {
+		_, err = rand.Read(bytes)
+		if err != nil {
+			return
+		}
+
+		numBig.SetBytes(bytes)
+		num := numBig.Uint64()
+
+		for num > 0 {
+			rm := int(num % base)
+			num = num / base
+			sb.WriteByte(alphabetDigit[rm])
+		}
+	}
+
+	s = sb.String()
+	if len(s) > limit {
+		s = s[:limit]
+	}
+
+	return
 }
--- yrand/randstr_test.go
+++ yrand/randstr_test.go
@@ -1,35 +1,35 @@
 package yrand
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestStringBase62(t *testing.T) {
-	s, e := StringBase62(10)
-	fmt.Println(s, e)
-	return
-
-	type args struct {
-		length int
-	}
 	tests := []struct {
-		name    string
-		args    args
-		wantS   string
-		wantErr bool
+		name       string
+		length     int
+		wantLength int
+		wantErr    bool
 	}{
-		// TODO: Add test cases.
+		{"negative limit value", -1, 0, true},
+		{"zero limit value", 0, 0, true},
+		{"set limit=1", 1, 1, false},
+		{"set limit=2", 2, 2, false},
+		{"set limit=8", 8, 8, false},
+		{"set limit=10", 10, 10, false},
+		{"set limit=16", 16, 16, false},
+		{"set limit=20", 20, 20, false},
+		{"set limit=40", 40, 40, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotS, err := StringBase62(tt.args.length)
+			gotS, err := StringBase62(tt.length)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("StringBase62() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if gotS != tt.wantS {
-				t.Errorf("StringBase62() gotS = %v, want %v", gotS, tt.wantS)
+			if len(gotS) != tt.wantLength {
+				t.Errorf("StringBase62() gotS = %v, len = %v, want %v", gotS, len(gotS), tt.wantLength)
 			}
 		})
 	}
