f6120ad36cf689220a19be42ac394a174b6d2238
Angel Analoosen <an9an63@gmail.com>
2019-10-13 20:54:16 +0800

    Complete tests for Choice*

--- yrand/randelem_test.go
+++ yrand/randelem_test.go
@@ -74,27 +74,41 @@ func BenchmarkShuffleWithSlice(b *testing.B) {
 }
 
 func TestChoiceInt(t *testing.T) {
-	type args struct {
-		list []int
-	}
+	int100, _ := rangeInt(100)
+	int500000, _ := rangeInt(500000)
 	tests := []struct {
-		name    string
-		args    args
-		wantN   int
-		wantErr bool
+		name          string
+		list          []int
+		checkContains bool
+		wantN         int
+		wantErr       bool
+		wantContains  bool
 	}{
-		{"nil list", args{nil}, 0, true},
-		{"empty list", args{[]int{}}, 0, true},
-		{"single item list", args{[]int{100}}, 100, false},
+		{"nil list", nil, false, 0, true, false},
+		{"empty list", []int{}, false, 0, true, false},
+		{"single item list", []int{100}, true, 100, false, true},
+		{"list of 2 - a", []int{100, 200}, true, 100, false, true},
+		{"list of 2 - b", []int{100, 200}, true, 100, false, true},
+		{"list of 3 - same", []int{300, 300, 300}, true, 300, false, true},
+		{"list of 5 - in", []int{100, 200, 300, 400, 500}, true, 300, false, true},
+		{"list of 5 - not", []int{100, 200, 300, 400, 500}, true, 350, false, false},
+		{"list of 100 - in", int100, true, 50, false, true},
+		{"list of 100 - not", int100, true, 100, false, false},
+		{"list of 500000 - in", int500000, true, 400000, false, true},
+		{"list of 500000 - not", int500000, true, 600000, false, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotN, err := ChoiceInt(tt.args.list)
+			gotN, err := ChoiceInt(tt.list)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("ChoiceInt() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if gotN != tt.wantN {
+			if tt.checkContains {
+				if contains := containInt(tt.list, tt.wantN); contains != tt.wantContains {
+					t.Errorf("ChoiceInt() gotN = %v, list = %v, contains actual: %v, expected: %v", gotN, tt.list, contains, tt.wantContains)
+				}
+			} else if gotN != tt.wantN {
 				t.Errorf("ChoiceInt() gotN = %v, want %v", gotN, tt.wantN)
 			}
 		})
@@ -102,27 +116,39 @@ func TestChoiceInt(t *testing.T) {
 }
 
 func TestChoiceString(t *testing.T) {
-	type args struct {
-		list []string
-	}
+	str100, _ := rangeString(100)
+	str500000, _ := rangeString(500000)
 	tests := []struct {
-		name    string
-		args    args
-		wantS   string
-		wantErr bool
+		name          string
+		list          []string
+		checkContains bool
+		wantS         string
+		wantErr       bool
+		wantContains  bool
 	}{
-		{"nil list", args{nil}, "", true},
-		{"empty list", args{[]string{}}, "", true},
-		{"single item list", args{[]string{"Good"}}, "Good", false},
+		{"nil list", nil, false, "", true, false},
+		{"empty list", []string{}, false, "", true, false},
+		{"single item list", []string{"Good"}, true, "Good", false, true},
+		{"list of 2 - a", []string{"Hello", "World"}, true, "Hello", false, true},
+		{"list of 2 - b", []string{"Hello", "World"}, true, "World", false, true},
+		{"list of 3 - same", []string{"Yes", "Yes", "Yes"}, true, "Yes", false, true},
+		{"list of 100 - in", str100, true, "50", false, true},
+		{"list of 100 - not", str100, true, "100", false, false},
+		{"list of 500000 - in", str500000, true, "400000", false, true},
+		{"list of 500000 - not", str500000, true, "600000", false, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotS, err := ChoiceString(tt.args.list)
+			gotS, err := ChoiceString(tt.list)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("ChoiceString() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if gotS != tt.wantS {
+			if tt.checkContains {
+				if contains := containString(tt.list, tt.wantS); contains != tt.wantContains {
+					t.Errorf("ChoiceString() gotS = %v, list = %v, contains actual: %v, expected: %v", gotS, tt.list, contains, tt.wantContains)
+				}
+			} else if gotS != tt.wantS {
 				t.Errorf("ChoiceString() gotS = %v, want %v", gotS, tt.wantS)
 			}
 		})
@@ -137,6 +163,18 @@ func rangeInt(max int) (sl []int, err error) {
 	sl = make([]int, 0, max)
 	for i := 0; i < max; i++ {
 		sl = append(sl, i)
+	}
+	return
+}
+
+func rangeString(max int) (sl []string, err error) {
+	if max < 0 {
+		return nil, errors.New("max should be non-negative")
+	}
+
+	sl = make([]string, 0, max)
+	for i := 0; i < max; i++ {
+		sl = append(sl, strconv.Itoa(i))
 	}
 	return
 }
@@ -159,4 +197,28 @@ func factorial(n int) int {
 		res *= i
 	}
 	return res
+}
+
+func containInt(list []int, n int) bool {
+	if len(list) <= 0 {
+		return false
+	}
+	for _, v := range list {
+		if v == n {
+			return true
+		}
+	}
+	return false
+}
+
+func containString(list []string, s string) bool {
+	if len(list) <= 0 {
+		return false
+	}
+	for _, v := range list {
+		if v == s {
+			return true
+		}
+	}
+	return false
 }
