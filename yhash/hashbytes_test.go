82add366dfabf79847f79fe3147cd10f4a2ccc5d
Angel Analoosen <53369766+an63@users.noreply.github.com>
2019-11-01 21:27:24 +0800

    Add SHA224 and SHA256 in `yhash` (#45)
    * Disable test-coverage analysis for DeepSource.io for its own issue
    * Implement SHA224/SHA256 methods
    * Add Tests for SHA224/SHA256
    * Use combined method for string/bytes/file hash

--- yhash/hashbytes_test.go
+++ yhash/hashbytes_test.go
@@ -4,80 +4,115 @@ import (
 	"testing"
 )
 
-var bytes3k = make([]byte, 0, 3000)
+var bytes4k = make([]byte, 0, 4096)
 
 func init() {
-	for i := 0; i < 1000; i++ {
-		bytes3k = append(bytes3k, 0x41, 0x42, 0x43)
-	}
-}
-
-func TestBytesMD5(t *testing.T) {
-	tests := []struct {
-		name    string
-		data    []byte
-		wantStr string
-		wantErr bool
-	}{
-		{"nil", nil, "d41d8cd98f00b204e9800998ecf8427e", false},
-		{"empty", []byte{}, "d41d8cd98f00b204e9800998ecf8427e", false},
-		{"one zero", []byte{0}, "93b885adfe0da089cdf634904fd59f71", false},
-		{"one byte", []byte{88}, "02129bb861061d1a052c592e2dc6b383", false},
-		{"two bytes", []byte{88, 89}, "74c53bcd3dcb2bb79993b2fec37d362a", false},
-		{"three bytes", []byte{88, 89, 90}, "e65075d550f9b5bf9992fa1d71a131be", false},
-		{"3k bytes", bytes3k, "0c101c2013f996d63f1f01450a4c73e9", false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gotStr, err := BytesMD5(tt.data)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("BytesMD5() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if gotStr != tt.wantStr {
-				t.Errorf("BytesMD5() gotStr = %v, want %v", gotStr, tt.wantStr)
-			}
-		})
+	for i := 0; i < 1024; i++ {
+		bytes4k = append(bytes4k, 0x46, 0x55, 0x43, 0x4b)
 	}
 }
 
 func BenchmarkBytesMD5(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		BytesMD5(bytes3k)
-	}
-}
-
-func TestBytesSHA1(t *testing.T) {
-	tests := []struct {
-		name    string
-		data    []byte
-		wantStr string
-		wantErr bool
-	}{
-		{"nil", nil, "da39a3ee5e6b4b0d3255bfef95601890afd80709", false},
-		{"empty", []byte{}, "da39a3ee5e6b4b0d3255bfef95601890afd80709", false},
-		{"one zero", []byte{0}, "5ba93c9db0cff93f52b521d7420e43f6eda2784f", false},
-		{"one byte", []byte{88}, "c032adc1ff629c9b66f22749ad667e6beadf144b", false},
-		{"two bytes", []byte{88, 89}, "034f1965ccdbdf9e642feeb9858da5096b6d1a9a", false},
-		{"three bytes", []byte{88, 89, 90}, "717c4ecc723910edc13dd2491b0fae91442619da", false},
-		{"3k bytes", bytes3k, "7cdb527beffa8ad0ffea8b0d6d6a997aa505cd27", false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gotStr, err := BytesSHA1(tt.data)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("BytesSHA1() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if gotStr != tt.wantStr {
-				t.Errorf("BytesSHA1() gotStr = %v, want %v", gotStr, tt.wantStr)
-			}
-		})
+		BytesMD5(bytes4k)
 	}
 }
 
 func BenchmarkBytesSHA1(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		BytesSHA1(bytes3k)
+		BytesSHA1(bytes4k)
+	}
+}
+
+func BenchmarkBytesSHA224(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		BytesSHA224(bytes4k)
+	}
+}
+
+func BenchmarkBytesSHA256(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		BytesSHA256(bytes4k)
+	}
+}
+
+func TestStringBytes(t *testing.T) {
+	type hashTestCase struct {
+		name    string
+		data    []byte
+		wantStr string
+		wantErr bool
+	}
+	tests := []struct {
+		name   string
+		method func(data []byte) (str string, err error)
+		cases  []hashTestCase
+	}{
+		{
+			name:   "MD5",
+			method: BytesMD5,
+			cases: []hashTestCase{
+				{"nil", nil, "d41d8cd98f00b204e9800998ecf8427e", false},
+				{"empty", []byte{}, "d41d8cd98f00b204e9800998ecf8427e", false},
+				{"one zero", []byte{0}, "93b885adfe0da089cdf634904fd59f71", false},
+				{"one byte", []byte{88}, "02129bb861061d1a052c592e2dc6b383", false},
+				{"two bytes", []byte{88, 89}, "74c53bcd3dcb2bb79993b2fec37d362a", false},
+				{"three bytes", []byte{88, 89, 90}, "e65075d550f9b5bf9992fa1d71a131be", false},
+				{"4k bytes", bytes4k, "f57c8ef3cb002cb6069be7c805f83ae4", false},
+			},
+		},
+		{
+			name:   "SHA1",
+			method: BytesSHA1,
+			cases: []hashTestCase{
+				{"nil", nil, "da39a3ee5e6b4b0d3255bfef95601890afd80709", false},
+				{"empty", []byte{}, "da39a3ee5e6b4b0d3255bfef95601890afd80709", false},
+				{"one zero", []byte{0}, "5ba93c9db0cff93f52b521d7420e43f6eda2784f", false},
+				{"one byte", []byte{88}, "c032adc1ff629c9b66f22749ad667e6beadf144b", false},
+				{"two bytes", []byte{88, 89}, "034f1965ccdbdf9e642feeb9858da5096b6d1a9a", false},
+				{"three bytes", []byte{88, 89, 90}, "717c4ecc723910edc13dd2491b0fae91442619da", false},
+				{"4k bytes", bytes4k, "695634e0d1baf3b99e3cae648414a7829d369f0d", false},
+			},
+		},
+		{
+			name:   "SHA224",
+			method: BytesSHA224,
+			cases: []hashTestCase{
+				{"nil", nil, "d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f", false},
+				{"empty", []byte{}, "d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f", false},
+				{"one zero", []byte{0}, "fff9292b4201617bdc4d3053fce02734166a683d7d858a7f5f59b073", false},
+				{"one byte", []byte{88}, "f00bdeb2cd9da240a57c951fdf1bcba509fd0cd83c5e5ad9a669de12", false},
+				{"two bytes", []byte{88, 89}, "a3a149bd66cd66e971d8ca4c12394818f6c63bca01a0d8c6b730f0d7", false},
+				{"three bytes", []byte{88, 89, 90}, "cc8660476871488742e0cac93a996a1b4fab7d3b7e3df10412cc0059", false},
+				{"4k bytes", bytes4k, "082947382dc751e6f5bbc59224b2758b7ce99627210715ba0ab6bced", false},
+			},
+		},
+		{
+			name:   "SHA256",
+			method: BytesSHA256,
+			cases: []hashTestCase{
+				{"nil", nil, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", false},
+				{"empty", []byte{}, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", false},
+				{"one zero", []byte{0}, "6e340b9cffb37a989ca544e6bb780a2c78901d3fb33738768511a30617afa01d", false},
+				{"one byte", []byte{88}, "4b68ab3847feda7d6c62c1fbcbeebfa35eab7351ed5e78f4ddadea5df64b8015", false},
+				{"two bytes", []byte{88, 89}, "c07a3de039fbc0914689549f041eae295d621de7f7f647fd863f6d2f8db2080e", false},
+				{"three bytes", []byte{88, 89, 90}, "ade099751d2ea9f3393f0f32d20c6b980dd5d3b0989dea599b966ae0d3cd5a1e", false},
+				{"4k bytes", bytes4k, "ba182851504af83589df0acd6ba850754d02cf61bff1ecd97ad810c34cfcdf79", false},
+			},
+		},
+	}
+	for _, ts := range tests {
+		for _, tt := range ts.cases {
+			t.Run(ts.name+" "+tt.name, func(t *testing.T) {
+				gotStr, err := ts.method(tt.data)
+				if (err != nil) != tt.wantErr {
+					t.Errorf("Bytes%s() error = %v, wantErr %v", ts.name, err, tt.wantErr)
+					return
+				}
+				if gotStr != tt.wantStr {
+					t.Errorf("Bytes%s() gotStr = %v, want %v", ts.name, gotStr, tt.wantStr)
+				}
+			})
+		}
 	}
 }
--- yhash/hashstring_test.go
+++ yhash/hashstring_test.go
@@ -5,33 +5,6 @@ import (
 	"testing"
 )
 
-func TestStringMD5(t *testing.T) {
-	tests := []struct {
-		name    string
-		content string
-		wantStr string
-		wantErr bool
-	}{
-		{"empty string", "", "d41d8cd98f00b204e9800998ecf8427e", false},
-		{"one-char string", "A", "7fc56270e7a70fa81a5935b72eacbe29", false},
-		{"str=12345678", "12345678", "25d55ad283aa400af464c76d713c07ad", false},
-		{"str=你好(*´▽｀)ノノ", "你好(*´▽｀)ノノ", "142e80fd38631675e5f19dcc3e81dc11", false},
-		{"long string", strings.Repeat("Good", 60), "7bbd7b0e70e71acbe1c4f0e67e59817e", false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gotStr, err := StringMD5(tt.content)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("StringMD5() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if gotStr != tt.wantStr {
-				t.Errorf("StringMD5() gotStr = %v, want %v", gotStr, tt.wantStr)
-			}
-		})
-	}
-}
-
 func BenchmarkStringMD5(b *testing.B) {
 	var content = strings.Repeat("Angel", 10)
 	b.ResetTimer()
@@ -40,37 +13,99 @@ func BenchmarkStringMD5(b *testing.B) {
 	}
 }
 
-func TestStringSHA1(t *testing.T) {
-	tests := []struct {
-		name    string
-		content string
-		wantStr string
-		wantErr bool
-	}{
-		{"empty string", "", "da39a3ee5e6b4b0d3255bfef95601890afd80709", false},
-		{"one-char string", "B", "ae4f281df5a5d0ff3cad6371f76d5c29b6d953ec", false},
-		{"str=123456789", "123456789", "f7c3bc1d808e04732adf679965ccc34ca7ae3441", false},
-		{"str=你好(*´▽｀)ノノ", "你好(*´▽｀)ノノ", "0780f2ed8873cc7ceff6d7925eea9992f6249b0f", false},
-		{"long string", strings.Repeat("Good", 60), "c8fee3477eb127f23cb7dcc9d96bd6cf96987c93", false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gotStr, err := StringSHA1(tt.content)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("StringSHA1() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if gotStr != tt.wantStr {
-				t.Errorf("StringSHA1() gotStr = %v, want %v", gotStr, tt.wantStr)
-			}
-		})
-	}
-}
-
 func BenchmarkStringSHA1(b *testing.B) {
 	var content = strings.Repeat("Angel", 10)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		StringSHA1(content)
+	}
+}
+
+func BenchmarkStringSHA224(b *testing.B) {
+	var content = strings.Repeat("Angel", 10)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		StringSHA224(content)
+	}
+}
+
+func BenchmarkStringSHA256(b *testing.B) {
+	var content = strings.Repeat("Angel", 10)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		StringSHA256(content)
+	}
+}
+
+func TestStringHash(t *testing.T) {
+	type hashTestCase struct {
+		name    string
+		content string
+		wantStr string
+		wantErr bool
+	}
+	tests := []struct {
+		name   string
+		method func(content string) (str string, err error)
+		cases  []hashTestCase
+	}{
+		{
+			name:   "MD5",
+			method: StringMD5,
+			cases: []hashTestCase{
+				{"empty string", "", "d41d8cd98f00b204e9800998ecf8427e", false},
+				{"one-char string", "A", "7fc56270e7a70fa81a5935b72eacbe29", false},
+				{"str=12345678", "12345678", "25d55ad283aa400af464c76d713c07ad", false},
+				{"str=你好(*´▽｀)ノノ", "你好(*´▽｀)ノノ", "142e80fd38631675e5f19dcc3e81dc11", false},
+				{"long string", strings.Repeat("Good", 60), "7bbd7b0e70e71acbe1c4f0e67e59817e", false},
+			},
+		},
+		{
+			name:   "SHA1",
+			method: StringSHA1,
+			cases: []hashTestCase{
+				{"empty string", "", "da39a3ee5e6b4b0d3255bfef95601890afd80709", false},
+				{"one-char string", "B", "ae4f281df5a5d0ff3cad6371f76d5c29b6d953ec", false},
+				{"str=123456789", "123456789", "f7c3bc1d808e04732adf679965ccc34ca7ae3441", false},
+				{"str=你好(*´▽｀)ノノ", "你好(*´▽｀)ノノ", "0780f2ed8873cc7ceff6d7925eea9992f6249b0f", false},
+				{"long string", strings.Repeat("Good", 60), "c8fee3477eb127f23cb7dcc9d96bd6cf96987c93", false},
+			},
+		},
+		{
+			name:   "SHA224",
+			method: StringSHA224,
+			cases: []hashTestCase{
+				{"empty string", "", "d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f", false},
+				{"one-char string", "C", "484d52691fcadbfabec5a318d1cf9692c7f293cbc8c1d5f22b2d839b", false},
+				{"str=123456789", "123456789", "9b3e61bf29f17c75572fae2e86e17809a4513d07c8a18152acf34521", false},
+				{"str=你好(*´▽｀)ノノ", "你好(*´▽｀)ノノ", "8cffb7f5b819a9131f42c67dbf8ab2f79c585e0a7d53c8948eccc435", false},
+				{"long string", strings.Repeat("Good", 70), "ac664b538d59ad4df4768830da3bbaf3a71eabed4aaa86c1074a8015", false},
+			},
+		},
+		{
+			name:   "SHA256",
+			method: StringSHA256,
+			cases: []hashTestCase{
+				{"empty string", "", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", false},
+				{"one-char string", "C", "6b23c0d5f35d1b11f9b683f0b0a617355deb11277d91ae091d399c655b87940d", false},
+				{"str=123456789", "123456789", "15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225", false},
+				{"str=你好(*´▽｀)ノノ", "你好(*´▽｀)ノノ", "140340b962fada1ba3ed7fbc28f14f58fad155d450ca7d9c6b82f9817c9f7275", false},
+				{"long string", strings.Repeat("Good", 70), "8b2428387ef532986b095492ce1afb949f797a125af101d655a5684c9fda6e8e", false},
+			},
+		},
+	}
+	for _, ts := range tests {
+		for _, tt := range ts.cases {
+			t.Run(ts.name+" "+tt.name, func(t *testing.T) {
+				gotStr, err := ts.method(tt.content)
+				if (err != nil) != tt.wantErr {
+					t.Errorf("String%s() error = %v, wantErr %v", ts.name, err, tt.wantErr)
+					return
+				}
+				if gotStr != tt.wantStr {
+					t.Errorf("String%s() gotStr = %v, want %v", ts.name, gotStr, tt.wantStr)
+				}
+			})
+		}
 	}
 }
