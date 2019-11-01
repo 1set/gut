6066ed8e3b0126639620903628df959ef1658860
Angel Analoosen <an9an63@gmail.com>
2019-11-01 13:55:20 +0800

    Test binary file

--- yhash/hashfile_test.go
+++ yhash/hashfile_test.go
@@ -1,6 +1,7 @@
 package yhash
 
 import (
+	"encoding/base64"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -17,19 +18,30 @@ func TestMain(m *testing.M) {
 var filePathMap = make(map[string]string)
 
 func setup() {
-	testFileContents := map[string]string{
+	tempFileContents := map[string]string{
 		"empty file":         "",
 		"one-line text file": "Hello World",
 		"large text file":    strings.Repeat("Stop managing your time. Start managing your focus. ", 10000),
 		"xlarge text file":   strings.Repeat("Do or do not, there is no try. ", 1000000),
+		"small binary file":  "R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==",
 	}
-	for title, content := range testFileContents {
+	for title, content := range tempFileContents {
 		name := strings.ReplaceAll(title, " ", "_")
 		if file, err := ioutil.TempFile("", name); err == nil {
-			if _, err = file.WriteString(content); err == nil {
-				filePathMap[title] = file.Name()
-				_ = file.Close()
+			if strings.Contains(title, "binary file") {
+				data, err := base64.StdEncoding.DecodeString(content)
+				if err != nil {
+					continue
+				}
+				if _, err = file.Write(data); err != nil {
+					continue
+				}
+			} else if _, err = file.WriteString(content); err != nil {
+				continue
 			}
+
+			filePathMap[title] = file.Name()
+			_ = file.Close()
 		}
 	}
 }
@@ -52,6 +64,7 @@ func TestFileMD5(t *testing.T) {
 		{"one-line text file", "", "b10a8db164e0754105b7a99be72e3fe5", false},
 		{"large text file", "", "3094ffc905b6a832d68ca27c86d52dc0", false},
 		{"xlarge text file", "", "0d9d7d9349c970fbf71b46698c5d1165", false},
+		{"small binary file", "", "f837aa60b6fe83458f790db60d529fc9", false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
