754a5e64ccac9c496c10254e0994095eb0d3ac77
Angel Analoosen <an9an63@gmail.com>
2019-11-01 18:20:13 +0800

    use 4k bytes for hash tests

--- yhash/hashbytes_test.go
+++ yhash/hashbytes_test.go
@@ -4,11 +4,11 @@ import (
 	"testing"
 )
 
-var bytes3k = make([]byte, 0, 3000)
+var bytes4k = make([]byte, 0, 4096)
 
 func init() {
-	for i := 0; i < 1000; i++ {
-		bytes3k = append(bytes3k, 0x41, 0x42, 0x43)
+	for i := 0; i < 1024; i++ {
+		bytes4k = append(bytes4k, 0x46, 0x55, 0x43, 0x4b)
 	}
 }
 
@@ -25,7 +25,7 @@ func TestBytesMD5(t *testing.T) {
 		{"one byte", []byte{88}, "02129bb861061d1a052c592e2dc6b383", false},
 		{"two bytes", []byte{88, 89}, "74c53bcd3dcb2bb79993b2fec37d362a", false},
 		{"three bytes", []byte{88, 89, 90}, "e65075d550f9b5bf9992fa1d71a131be", false},
-		{"3k bytes", bytes3k, "0c101c2013f996d63f1f01450a4c73e9", false},
+		{"4k bytes", bytes4k, "f57c8ef3cb002cb6069be7c805f83ae4", false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -43,7 +43,7 @@ func TestBytesMD5(t *testing.T) {
 
 func BenchmarkBytesMD5(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		BytesMD5(bytes3k)
+		BytesMD5(bytes4k)
 	}
 }
 
@@ -60,7 +60,7 @@ func TestBytesSHA1(t *testing.T) {
 		{"one byte", []byte{88}, "c032adc1ff629c9b66f22749ad667e6beadf144b", false},
 		{"two bytes", []byte{88, 89}, "034f1965ccdbdf9e642feeb9858da5096b6d1a9a", false},
 		{"three bytes", []byte{88, 89, 90}, "717c4ecc723910edc13dd2491b0fae91442619da", false},
-		{"3k bytes", bytes3k, "7cdb527beffa8ad0ffea8b0d6d6a997aa505cd27", false},
+		{"4k bytes", bytes4k, "695634e0d1baf3b99e3cae648414a7829d369f0d", false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -78,6 +78,6 @@ func TestBytesSHA1(t *testing.T) {
 
 func BenchmarkBytesSHA1(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		BytesSHA1(bytes3k)
+		BytesSHA1(bytes4k)
 	}
 }
