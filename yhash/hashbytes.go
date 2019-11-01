abbb814a42ab7968443a45bcf42914af9650549d
Angel Analoosen <an9an63@gmail.com>
2019-11-01 15:46:36 +0800

    Add N.B. warning for docs

--- yhash/hashbytes.go
+++ yhash/hashbytes.go
@@ -6,12 +6,14 @@ import (
 )
 
 // BytesMD5 returns MD5 checksum of the given bytes.
+// N.B. MD5 is cryptographically broken and should not be used for secure applications.
 func BytesMD5(data []byte) (str string, err error) {
 	hash := md5.New()
 	return calculateBytesHash(&hash, data)
 }
 
-// BytesSHA1 returns SHA1 checksum of the given bytes.
+// BytesSHA1 returns SHA-1 checksum of the given bytes.
+// N.B. SHA-1 is cryptographically broken and should not be used for secure applications.
 func BytesSHA1(data []byte) (str string, err error) {
 	hash := sha1.New()
 	return calculateBytesHash(&hash, data)
--- yhash/hashfile.go
+++ yhash/hashfile.go
@@ -6,12 +6,14 @@ import (
 )
 
 // FileMD5 returns MD5 checksum of the named file.
+// N.B. MD5 is cryptographically broken and should not be used for secure applications.
 func FileMD5(filePath string) (str string, err error) {
 	hash := md5.New()
 	return calculateFileHash(&hash, filePath)
 }
 
-// FileSHA1 returns SHA1 checksum of the named file.
+// FileSHA1 returns SHA-1 checksum of the named file.
+// N.B. SHA-1 is cryptographically broken and should not be used for secure applications.
 func FileSHA1(filePath string) (str string, err error) {
 	hash := sha1.New()
 	return calculateFileHash(&hash, filePath)
