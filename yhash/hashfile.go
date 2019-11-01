a9aa7e4a65c0be3f9489b3e9439b74219aff9a05
Angel Analoosen <an9an63@gmail.com>
2019-11-01 14:48:14 +0800

    Add test and code for SHA1

--- yhash/hashfile.go
+++ yhash/hashfile.go
@@ -2,10 +2,17 @@ package yhash
 
 import (
 	"crypto/md5"
+	"crypto/sha1"
 )
 
 // FileMD5 returns MD5 checksum of the named file.
 func FileMD5(filePath string) (str string, err error) {
 	hash := md5.New()
+	return calculateFileHash(&hash, filePath)
+}
+
+// FileSHA1 returns SHA1 checksum of the named file.
+func FileSHA1(filePath string) (str string, err error) {
+	hash := sha1.New()
 	return calculateFileHash(&hash, filePath)
 }
--- yhash/hashfile_test.go
+++ yhash/hashfile_test.go
@@ -102,3 +102,53 @@ func BenchmarkFileMD5(b *testing.B) {
 		FileMD5(path)
 	}
 }
+
+func TestFileSHA1(t *testing.T) {
+	tests := []struct {
+		name     string
+		filePath string
+		want     string
+		wantErr  bool
+	}{
+		{"file not found", "__FILE__NOT__EXIST__", "", true},
+		{"empty file", "", "da39a3ee5e6b4b0d3255bfef95601890afd80709", false},
+		{"one-line text file", "", "0a4d55a8d778e5022fab701977c5d840bbc486d0", false},
+		{"large text file", "", "22c538b3b8ba9a9f817eb05d463a0cb7ba6a9625", false},
+		{"xlarge text file", "", "3912642ec8f1430ae3f1f870d2279a26a0a02297", false},
+		{"small binary file", "", "14af87ccec7f81bb28d53c84da2fd5a9d5925cda", false},
+		{"another small binary", "", "cd2ad24ee27178115ff6440bb3f996a142888838", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			filePath := tt.filePath
+			if len(tt.filePath) == 0 {
+				var found bool
+				if filePath, found = filePathMap[tt.name]; !found {
+					t.Errorf("FileSHA1() got no file for case '%v'", tt.name)
+					return
+				}
+			}
+
+			got, err := FileSHA1(filePath)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FileSHA1() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("FileSHA1() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkFileSHA1(b *testing.B) {
+	path, found := "", false
+	if path, found = filePathMap["large text file"]; !found {
+		b.Errorf("FileSHA1() got no file for benchmark")
+		return
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		FileSHA1(path)
+	}
+}
