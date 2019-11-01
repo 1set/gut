6b37bc63624826b9968316f907685ee38d068395
Angel Analoosen <an9an63@gmail.com>
2019-11-01 14:27:20 +0800

    Add another test for binary file

--- yhash/hashfile_test.go
+++ yhash/hashfile_test.go
@@ -19,16 +19,17 @@ var filePathMap = make(map[string]string)
 
 func setup() {
 	tempFileContents := map[string]string{
-		"empty file":         "",
-		"one-line text file": "Hello World",
-		"large text file":    strings.Repeat("Stop managing your time. Start managing your focus. ", 10000),
-		"xlarge text file":   strings.Repeat("Do or do not, there is no try. ", 1000000),
-		"small binary file":  "R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==",
+		"empty file":           "",
+		"one-line text file":   "Hello World",
+		"large text file":      strings.Repeat("Stop managing your time. Start managing your focus. ", 10000),
+		"xlarge text file":     strings.Repeat("Do or do not, there is no try. ", 1000000),
+		"small binary file":    "R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==",
+		"another small binary": "VGhlIHF1aWNrIGJyb3duIPCfpooganVtcHMgb3ZlciAxMyBsYXp5IPCfkLYuIOKAnFRoZXJlIHdhcyBhIHNtYWxsIG51bWJlciBvZiByZWFsbHkgc21hcnQsIHJlYWxseSB5b3VuZyBjb2RlcnMgd2hvIHByb2R1Y2VkIGEgbG90IG9mIHZlcnkgY2xldmVyIGNvZGUgdGhhdCBvbmx5IHRoZXkgY291bGQgdW5kZXJzdGFuZCzigJ0gc2FpZCB2YW4gUm9zc3VtLiDigJxUaGF0IGlzIHByb2JhYmx5IHRoZSByaWdodCBhdHRpdHVkZSB0byBoYXZlIHdoZW4geW91J3JlIGEgcmVhbGx5IHNtYWxsIHN0YXJ0dXAu4oCd",
 	}
 	for title, content := range tempFileContents {
 		name := strings.ReplaceAll(title, " ", "_")
 		if file, err := ioutil.TempFile("", name); err == nil {
-			if strings.Contains(title, "binary file") {
+			if strings.Contains(title, "binary") {
 				data, err := base64.StdEncoding.DecodeString(content)
 				if err != nil {
 					continue
@@ -65,6 +66,7 @@ func TestFileMD5(t *testing.T) {
 		{"large text file", "", "3094ffc905b6a832d68ca27c86d52dc0", false},
 		{"xlarge text file", "", "0d9d7d9349c970fbf71b46698c5d1165", false},
 		{"small binary file", "", "f837aa60b6fe83458f790db60d529fc9", false},
+		{"another small binary", "", "89e19cf9c9680994d75adfac30887b31", false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
