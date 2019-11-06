611b75aec7823170bcef5109e0f3901c31cb7eab
Angel Analoosen <an9an63@gmail.com>
2019-11-06 13:47:48 +0800

    Fix example

--- yhash/example_test.go
+++ yhash/example_test.go
@@ -15,5 +15,28 @@ func ExampleFileMD5() {
 	}
 
 	fmt.Println("MD5:", hash)
-	// Output: MD5: 3d34538da89e40ff9cb90ca9abe9e45c
+}
+
+// This example calculates SHA-1 checksum of a given string.
+func ExampleStringSHA1() {
+	hash, err := yhash.StringSHA1("This page is intentionally left blank.")
+	if err != nil {
+		fmt.Println("got error:", err)
+		return
+	}
+
+	fmt.Println("SHA1:", hash)
+	// Output: SHA1: dfca586074b7cfd24abc19fee213de5558e57422
+}
+
+// This example calculates SHA-256 checksum of a slice of bytes.
+func ExampleBytesSHA256() {
+	hash, err := yhash.BytesSHA256([]byte("This page unintentionally left blank."))
+	if err != nil {
+		fmt.Println("got error:", err)
+		return
+	}
+
+	fmt.Println("SHA256:", hash)
+	// Output: SHA256: 19e25cb9879fa4510e6ebfacc8836597429da7d5b279ccc073249492de2eae10
 }
