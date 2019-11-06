443a5af0be56188f61236b3bb70e0df9a8f674bb
Angel Analoosen <53369766+an63@users.noreply.github.com>
2019-11-06 14:09:32 +0800

    Add Example for yhash (#53)
    * Update test and docs for yrand
    * Add yhash/example_test.go
    * Add doc strings for internal shared methods

---
+++ yhash/example_test.go
@@ -0,0 +1,42 @@
+package yhash_test
+
+import (
+	"fmt"
+
+	"github.com/an63/gut/yhash"
+)
+
+// This example calculates MD5 checksum of "doc.go" file in current folder.
+func ExampleFileMD5() {
+	hash, err := yhash.FileMD5("doc.go")
+	if err != nil {
+		fmt.Println("got error:", err)
+		return
+	}
+
+	fmt.Println("MD5:", hash)
+}
+
+// This example calculates SHA-1 checksum of a given string.
+func ExampleStringSHA1() {
+	hash, err := yhash.StringSHA1("This page is intentionally left blank.")
+	if err != nil {
+		fmt.Println("got error:", err)
+		return
+	}
+
+	fmt.Println("SHA1:", hash)
+	// Output: SHA1: dfca586074b7cfd24abc19fee213de5558e57422
+}
+
+// This example calculates SHA-256 checksum of a slice of bytes.
+func ExampleBytesSHA256() {
+	hash, err := yhash.BytesSHA256([]byte("This page unintentionally left blank."))
+	if err != nil {
+		fmt.Println("got error:", err)
+		return
+	}
+
+	fmt.Println("SHA256:", hash)
+	// Output: SHA256: 19e25cb9879fa4510e6ebfacc8836597429da7d5b279ccc073249492de2eae10
+}
