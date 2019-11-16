3598e038618a65b4faf95b6b544cf39a47ffbb0e
Angel Analoosen <an9an63@gmail.com>
2019-11-16 14:21:05 +0800

    Add TrimAfterLast()

--- ystring/trim.go
+++ ystring/trim.go
@@ -17,3 +17,17 @@ func TrimAfterFirst(s, substr string) string {
 	}
 	return s
 }
+
+func TrimAfterLast(s, substr string) string {
+	switch ls, lsub := len(s), len(substr); {
+	case ls == 0 || lsub == 0:
+		return s
+	case ls < lsub:
+		return s
+	}
+
+	if idx := strings.LastIndex(s, substr); idx >= 0 {
+		return s[0:idx]
+	}
+	return s
+}
--- ystring/trim_test.go
+++ ystring/trim_test.go
@@ -49,3 +49,42 @@ func BenchmarkTrimAfterFirst(b *testing.B) {
 		_ = TrimAfterFirst("Long, long ago, long ago, long ago, long ago.", "long ago")
 	}
 }
+
+func TestTrimAfterLast(t *testing.T) {
+	type args struct {
+		s      string
+		substr string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Empty string", args{emptyString, threeCharsString}, emptyString},
+		{"Empty substring", args{threeCharsString, emptyString}, threeCharsString},
+		{"Empty string and substring", args{emptyString, emptyString}, emptyString},
+		{"Same string and substring", args{threeCharsString, threeCharsString}, emptyString},
+		{"Equal length with diff content", args{"ABC", "ABD"}, "ABC"},
+		{"Substring contains string", args{"ABC", "ABCDE"}, "ABC"},
+		{"String starts with substring", args{"AppleComputer", "Apple"}, emptyString},
+		{"String ends with substring", args{"AppleComputer", "Computer"}, "Apple"},
+		{"String doesn't contain substring", args{"AppleComputer", "Banana"}, "AppleComputer"},
+		{"String contains single substring", args{"What a wonderful world!", "wonderful"}, "What a "},
+		{"String contains one-char substring", args{"abc.def.ghi.txt", "."}, "abc.def.ghi"},
+		{"String contains multiple substring", args{"Long, long ago, long ago, long ago, long ago.", "long ago"}, "Long, long ago, long ago, long ago, "},
+		{"String and substring contains non-ASCII", args{"我真的非常非常感谢你🤙", "非常"}, "我真的非常"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TrimAfterLast(tt.args.s, tt.args.substr); got != tt.want {
+				t.Errorf("TrimAfterFirst() = %q, want %q", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkTrimAfterLast(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = TrimAfterLast("Long, long ago, long ago, long ago, long ago.", "long ago")
+	}
+}
