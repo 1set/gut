c0c64aa1199b7342e656fb93b0b9e626525e6cbb
Angel Analoosen <an9an63@gmail.com>
2019-11-16 15:28:49 +0800

    Implement TrimBefore First/Last

--- ystring/trim.go
+++ ystring/trim.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 )
 
-// TrimAfterFirst returns s without the part after the first instance of substr.
+// TrimAfterFirst returns s without the part after the first instance of substr and that instance itself.
 // If substr is empty or not present in s, s is returned unchanged.
 func TrimAfterFirst(s, substr string) string {
 	switch ls, lsub := len(s), len(substr); {
@@ -20,7 +20,7 @@ func TrimAfterFirst(s, substr string) string {
 	return s
 }
 
-// TrimAfterLast returns s without the part after the last instance of substr.
+// TrimAfterLast returns s without the part after the last instance of substr and that instance itself.
 // If substr is empty or not present in s, s is returned unchanged.
 func TrimAfterLast(s, substr string) string {
 	switch ls, lsub := len(s), len(substr); {
@@ -32,6 +32,40 @@ func TrimAfterLast(s, substr string) string {
 
 	if idx := strings.LastIndex(s, substr); idx >= 0 {
 		return s[0:idx]
+	}
+	return s
+}
+
+// TrimBeforeFirst returns s without the part before the first instance of substr and that instance itself.
+// If substr is empty or not present in s, s is returned unchanged.
+func TrimBeforeFirst(s, substr string) string {
+	ls, lsub := len(s), len(substr)
+	switch {
+	case ls == 0 || lsub == 0:
+		return s
+	case ls < lsub:
+		return s
+	}
+
+	if idx := strings.Index(s, substr); idx >= 0 {
+		return s[idx+lsub:]
+	}
+	return s
+}
+
+// TrimBeforeLast returns s without the part before the last instance of substr and that instance itself.
+// If substr is empty or not present in s, s is returned unchanged.
+func TrimBeforeLast(s, substr string) string {
+	ls, lsub := len(s), len(substr)
+	switch {
+	case ls == 0 || lsub == 0:
+		return s
+	case ls < lsub:
+		return s
+	}
+
+	if idx := strings.LastIndex(s, substr); idx >= 0 {
+		return s[idx+lsub:]
 	}
 	return s
 }
