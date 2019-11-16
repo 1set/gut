a41eff5aa4c9484e91463ef54e50b0cb0907cd7a
Angel Analoosen <an9an63@gmail.com>
2019-11-16 12:38:39 +0800

    Add tests for TrimAfterFirst

--- ystring/trim_test.go
+++ ystring/trim_test.go
@@ -4,6 +4,12 @@ import (
 	"testing"
 )
 
+var (
+	oneCharString    = "A"
+	twoCharsString   = "KO"
+	threeCharsString = "Luv"
+)
+
 func TestTrimAfterFirst(t *testing.T) {
 	type args struct {
 		s      string
@@ -14,7 +20,17 @@ func TestTrimAfterFirst(t *testing.T) {
 		args args
 		want string
 	}{
-		// TODO: Add test cases.
+		{"Empty string", args{emptyString, threeCharsString}, emptyString},
+		{"Empty substring", args{threeCharsString, emptyString}, threeCharsString},
+		{"Empty string and substring", args{emptyString, emptyString}, emptyString},
+		{"Same string and substring", args{threeCharsString, threeCharsString}, emptyString},
+		{"Equal length with diff content", args{"ABC", "ABD"}, "ABC"},
+		{"Substring contains string", args{"ABC", "ABCDE"}, "ABC"},
+		{"String starts with substring", args{"AppleComputer", "Apple"}, emptyString},
+		{"String ends with substring", args{"AppleComputer", "Computer"}, "Apple"},
+		{"String doesn't contain substring", args{"AppleComputer", "Banana"}, "AppleComputer"},
+		{"String contains multiple substring", args{"Long, long ago, long ago, long ago, long ago.", "long ago"}, "Long, "},
+		{"String and substring contains non-ASCII", args{"我真的非常非常感谢你🤙", "非常"}, "我真的"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -22,5 +38,11 @@ func TestTrimAfterFirst(t *testing.T) {
 				t.Errorf("TrimAfterFirst() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func BenchmarkTrimAfterFirst(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = TrimAfterFirst("Long, long ago, long ago, long ago, long ago.", "long ago")
 	}
 }
