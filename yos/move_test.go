b6af43ae727375c2510274adf937d92473eb8678
Angel Analoosen <an9an63@gmail.com>
2020-01-29 14:14:23 +0800

    Add test case names for movefile

--- yos/move_test.go
+++ yos/move_test.go
@@ -41,7 +41,55 @@ func TestMoveFile(t *testing.T) {
 		outputPath string
 		wantErr    bool
 	}{
-		{"Sample", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		// {"Sample", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+
+		{"Source is empty", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Source doesn't exist", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Source is a symlink to file", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Source is a symlink to directory", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Source is a circular symlink", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Source is a broken symlink", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Source got no permissions", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+
+		{"Destination is empty", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Destination doesn't exist but its parent directory does", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Destination and its parent directory don't exist", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Destination is a symlink to file", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Destination is a symlink to directory", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Destination is a file", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Destination is a directory containing other files", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Destination is a directory containing nothing", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Destination is a directory containing a file with the same name", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Destination is a circular symlink", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Destination is a broken symlink", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Destination file got no permissions", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Destination directory got no permissions", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+
+		{"Source and destination is the same file", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Source and destination is the same symlink", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Source and destination is the same directory", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+
+		{"Cross-device: destination doesn't exist but its parent directory does", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Cross-device: destination and its parent directory don't exist", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Cross-device: destination exists and is a symlink to file", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Cross-device: destination exists and is a symlink to directory", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Cross-device: destination exists and is a file", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Cross-device: destination exists and is a directory", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Cross-device: destination exists and is a circular symlink", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Cross-device: destination exists and is a broken symlink", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Cross-device: destination file got no permissions", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Cross-device: destination directory got no permissions", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Cross-device: destination got no spaces for large file", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Cross-device: destination is a read-only device", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+
+		{"Rename: move empty file", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Rename: move small text file", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Rename: move large text file", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Rename: move image file", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Cross-device: move empty file", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Cross-device: move small text file", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Cross-device: move large text file", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
+		{"Cross-device: move image file", JoinPath(srcRoot, "text.txt"), JoinPath(destRoot, "text.txt"), JoinPath(bkRoot, "text.txt"), JoinPath(destRoot, "text.txt"), false},
 	}
 
 	for _, tt := range tests {
