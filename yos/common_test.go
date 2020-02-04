fc7b4a8eaaed9a61d8c0b4d74c2cf6e8f7bde149
Angel Analoosen <an9an63@gmail.com>
2020-02-05 02:12:13 +0800

    Fix

--- yos/common_test.go
+++ yos/common_test.go
@@ -114,6 +114,8 @@ func Test_resolveDirInfo(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			preconditionCheck(t, tt.name)
+
 			gotPath, gotFi, err := resolveDirInfo(tt.pathRaw)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("resolveDirInfo() error = %v, wantErr %v", err, tt.wantErr)
