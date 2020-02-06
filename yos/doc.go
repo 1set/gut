7e913c32e31d56e9476713b669c3293813759029
Angel Analoosen <an9an63@gmail.com>
2020-02-06 18:15:20 +0800

    Update yos/docs.go

--- yos/doc.go
+++ yos/doc.go
@@ -1,4 +1,31 @@
 /*
 Package yos is yet another wrapper of platform-independent interface to operating system functionality.
+
+
+System Info
+
+Check architecture of current system:
+  - IsOn32bitArch
+  - IsOn64bitArch
+
+Check current operating system:
+  - IsOnLinux
+  - IsOnMacOS
+  - IsOnMacOS
+
+
+File Operations
+
+Overview:
+
+	| Operation   | Directory      | File            | Symbolic Link      |
+	| ----------- | -------------- | --------------- | ------------------ |
+	| Check Exist | ExistDir       | ExistFile       | ExistSymlink       |
+	| Check Empty | IsDirEmpty     | IsFileEmpty     | -                  |
+	| Check Same  | SameDirEntries | SameFileContent | SameSymlinkContent |
+	| Get Size    | GetDirSize     | GetFileSize     | GetSymlinkSize     |
+	| Copy        | CopyDir        | CopyFile        | CopySymlink        |
+	| Move        | MoveDir        | MoveFile        | MoveSymlink        |
+
 */
 package yos
