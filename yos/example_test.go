56e531a566a8a848c09b8e17d33f8ca0e8e6bd57
Angel Analoosen <an9an63@gmail.com>
2020-02-07 18:51:56 +0800

    Add example misc

--- yos/example_test.go
+++ yos/example_test.go
@@ -2,9 +2,41 @@ package yos_test
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/1set/gut/yos"
 )
+
+// This example moves all image files from sub-folders of the source directory to the same destination directory.
+func Example() {
+	srcRoot := "source"
+	destRoot := "collect"
+
+	srcFiles, err := yos.ListMatch(srcRoot, yos.ListIncludeFile|yos.ListRecursive|yos.ListToLower, "*.jpg", "*.jpeg", "*.png", "*.gif")
+	if err != nil {
+		fmt.Printf("fail to list %q: %v\n", srcRoot, err)
+		return
+	}
+	sort.Stable(yos.SortListByModTime(srcFiles))
+
+	cntMove, cntSkip := 0, 0
+	for _, src := range srcFiles {
+		destPath := yos.JoinPath(destRoot, src.Info.Name())
+		// skip if the same file already exists
+		if yos.ExistFile(destPath) {
+			if same, err := yos.SameFileContent(src.Path, destPath); same && err == nil {
+				cntSkip++
+				continue
+			}
+		}
+
+		if err := yos.MoveFile(src.Path, destPath); err == nil {
+			cntMove++
+		}
+	}
+
+	fmt.Printf("total: %d\nmove : %d\nskip : %d\nfail : %d\n", len(srcFiles), cntMove, cntSkip, len(srcFiles)-cntMove-cntSkip)
+}
 
 // This example lists all files in the current directory and its sub-directories.
 func ExampleListFile() {
