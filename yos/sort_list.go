b98a63411ab5bb64452158c78b396f274df9b886
Angel Analoosen <an9an63@gmail.com>
2020-02-07 15:13:25 +0800

    Add draft for yos/list sortings

---
+++ yos/sort_list.go
@@ -0,0 +1,43 @@
+package yos
+
+type SortListByName []*FilePathInfo
+
+func (n SortListByName) Len() int {
+	return len(n)
+}
+
+func (n SortListByName) Less(i, j int) bool {
+	return n[i].Info.Name() < n[j].Info.Name()
+}
+
+func (n SortListByName) Swap(i, j int) {
+	n[i], n[j] = n[j], n[i]
+}
+
+type SortListBySize []*FilePathInfo
+
+func (s SortListBySize) Len() int {
+	return len(s)
+}
+
+func (s SortListBySize) Less(i, j int) bool {
+	return s[i].Info.Size() < s[j].Info.Size()
+}
+
+func (s SortListBySize) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+type SortListByModTime []*FilePathInfo
+
+func (t SortListByModTime) Len() int {
+	return len(t)
+}
+
+func (t SortListByModTime) Less(i, j int) bool {
+	return t[i].Info.ModTime().Before(t[j].Info.ModTime())
+}
+
+func (t SortListByModTime) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
