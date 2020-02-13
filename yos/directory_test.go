34c3d06c29e67acaa1f2172b7c56f8a0df4d050c
Angel Analoosen <an9an63@gmail.com>
2020-02-13 22:40:43 +0800

    Add tests for mkdir

--- yos/directory_test.go
+++ yos/directory_test.go
@@ -54,3 +54,35 @@ func BenchmarkChExeDir(b *testing.B) {
 		_ = ChExeDir()
 	}
 }
+
+func TestMakeDir(t *testing.T) {
+	tests := []struct {
+		name    string
+		path    string
+		wantErr bool
+	}{
+		{"Path is empty", "", true},
+		{"Path dir exists", resourceExistRoot, false},
+		{"Path dir doesn't exist but its parent does", JoinPath(resourceExistRoot, "mkdir-new-dir1"), false},
+		{"Path dir and its parent don't exist", JoinPath(resourceExistRoot, "mkdir-new-dir2", "mkdir-new-dir2b", "mkdir-new-dir2c"), false},
+		{"Path file exists", JoinPath(resourceExistRoot, "origin_file.txt"), true},
+		{"Path symlink exists", JoinPath(resourceExistRoot, "symlink.txt"), true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := MakeDir(tt.path); (err != nil) != tt.wantErr {
+				t.Errorf("MakeDir() error = %v, wantErr %v", err, tt.wantErr)
+			} else {
+				expectedErrorCheck(t, err)
+			}
+		})
+	}
+}
+
+func BenchmarkMakeDir(b *testing.B) {
+	path := JoinPath(resourceExistRoot, "mkdir-bench-new")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = MakeDir(path)
+	}
+}
