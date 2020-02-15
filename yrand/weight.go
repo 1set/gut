9414778ebefaaa320a39681fcf2a503af88a7dda
Angel Analoosen <an9an63@gmail.com>
2020-02-15 21:51:11 +0800

    Add WeightedShuffle and its test

--- yrand/weight.go
+++ yrand/weight.go
@@ -2,6 +2,8 @@ package yrand
 
 import (
 	"errors"
+	"fmt"
+	"sort"
 )
 
 var (
@@ -40,5 +42,51 @@ func WeightedChoice(weights []float64) (idx int, err error) {
 	}
 
 	idx = count - 1
+	return
+}
+
+func WeightedShuffle(weights []float64, yield func(idx int) (err error)) (err error) {
+	var (
+		count   = len(weights)
+		cumSum  = make([]float64, 0, count)
+		sum     = 0.0
+		randNum float64
+	)
+	if count <= 0 {
+		err = fmt.Errorf("empty weight list")
+		return
+	}
+	for _, weight := range weights {
+		// add check here for weights like [1e-6, 1e30, 1e-3],
+		if (weight <= 0) || (sum+weight == sum) || (sum-weight == sum) {
+			err = fmt.Errorf("invalid weight 1&2")
+			return
+		}
+		sum += weight
+		cumSum = append(cumSum, sum)
+	}
+
+	for range weights {
+		// get random pos
+		if randNum, err = Float64(); err != nil {
+			break
+		}
+		randNum *= cumSum[count-1]
+
+		// binary search for the pos and yield it
+		j := sort.Search(count, func(i int) bool { return cumSum[i] > randNum })
+		if !((0 <= j) && (j < count)) {
+			err = fmt.Errorf("invalid weight 3")
+			break
+		}
+		if err = yield(j); err != nil {
+			break
+		}
+
+		// remove weight from rest of the sum list
+		for p := j; p < count; p++ {
+			cumSum[p] -= weights[j]
+		}
+	}
 	return
 }
--- yrand/weight_test.go
+++ yrand/weight_test.go
@@ -1,6 +1,7 @@
 package yrand
 
 import (
+	"fmt"
 	"math"
 	"testing"
 )
@@ -16,14 +17,15 @@ func TestWeightedChoice(t *testing.T) {
 	}{
 		{"nil", nil, true},
 		{"empty weights", []float64{}, true},
-		{"non-positive weights", []float64{0, 0, -1}, true},
+		{"only zero weights", []float64{0, 0}, true},
+		{"only non-positive weights", []float64{0, 0, -1}, true},
 		{"single weight", []float64{1}, false},
-		{"two equal weights", []float64{2, 2}, false},
 		{"two diff weights", []float64{1, 3}, false},
+		{"two equal weights", []float64{2, 2}, false},
 		{"three diff weights", []float64{2, 4, 4}, false},
 		{"contains one larger weight", []float64{1, 100, 1}, false},
 		{"contains two larger weights", []float64{1, 100, 100}, false},
-		{"contains non-positive weights", []float64{10, 0, 10}, false},
+		{"contains non-positive weight", []float64{10, 0, 10}, false},
 		{"contains non-positive weights", []float64{-1, 10, 0}, false},
 		{"three increasing weights", []float64{1, 100, 1000}, false},
 		{"four increasing weights", []float64{2.333, 4.666, 8.888, 10.101}, false},
@@ -49,6 +51,60 @@ func TestWeightedChoice(t *testing.T) {
 	}
 }
 
+func TestWeightedShuffle(t *testing.T) {
+	var (
+		times = 500000
+	)
+	tests := []struct {
+		name    string
+		weights []float64
+		wantErr bool
+	}{
+		{"nil", nil, true},
+		{"empty weights", []float64{}, true},
+		{"only zero weights", []float64{0, 0}, true},
+		{"only non-positive weights", []float64{0, 0, -1}, true},
+		{"contains non-positive weights", []float64{-1, 10, 0}, false},
+		{"contains extremely larger weight", []float64{1e-6, 1e30, 1e-3, 1}, true},
+		{"single weight", []float64{1}, false},
+		{"two diff weights", []float64{1, 3}, false},
+		{"two equal weights", []float64{2, 2}, false},
+		{"three diff weights", []float64{2, 4, 4}, false},
+		{"contains one larger weight", []float64{1, 100, 1}, false},
+		{"contains two larger weights", []float64{1, 100, 100}, false},
+		{"three increasing weights", []float64{1, 100, 1000}, false},
+		{"four increasing weights", []float64{2.333, 4.666, 8.888, 10.101}, false},
+		{"five increasing weights", []float64{1, 2, 3, 4, 5}, false},
+		{"six repeated weights", []float64{1, 2, 1, 2, 1, 2}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := WeightedShuffle(tt.weights, func(idx int) (err error) {
+				return
+			}); (err != nil) != tt.wantErr {
+				t.Errorf("WeightedShuffle() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if !tt.wantErr {
+				checkProbDist(t, "WeightedShuffle", times, tt.weights, func() (idx int, err error) {
+					savedIdx, cnt := 0, 0
+					err = WeightedShuffle(tt.weights, func(idx int) error {
+						if cnt == 0 {
+							savedIdx = idx
+						}
+						if !(0 <= idx && idx < len(tt.weights)) {
+							return fmt.Errorf("invalid index: %v", idx)
+						}
+						cnt++
+						return nil
+					})
+					return savedIdx, err
+				})
+			}
+		})
+	}
+}
+
 func checkProbDist(t *testing.T, name string, times int, weights []float64, idxFunc func() (idx int, err error)) {
 	var (
 		tolerance        = 0.125
@@ -62,6 +118,11 @@ func checkProbDist(t *testing.T, name string, times int, weights []float64, idxF
 			t.Errorf("%s() got unexpected error = %v", name, err)
 			return
 		}
+		if !(0 <= idx && idx < len(weights)) {
+			t.Errorf("%s() got invalid index = %v, want = [0, %v)", name, idx, len(weights))
+			return
+		}
+
 		if _, ok := result[idx]; ok {
 			result[idx] += 1
 		} else {
