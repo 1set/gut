471e29baa78bc1e2722c84647adde59e8d613594
Angel ☕ <53369766+an63@users.noreply.github.com>
2020-02-19 09:00:47 +0800

    Replace shuffle algorithm for WeightedShuffle() (#80)
    * Repro weight issues
    * Use linked list instead
    * Improve tests for yrand
    * Short mode and upgrade Go version

--- yrand/weight_test.go
+++ yrand/weight_test.go
@@ -3,6 +3,7 @@ package yrand
 import (
 	"errors"
 	"fmt"
+	"math"
 	"testing"
 )
 
@@ -32,6 +33,8 @@ func TestWeightedChoice(t *testing.T) {
 		{"three increasing weights", []float64{1, 100, 1000}, false},
 		{"four increasing weights", []float64{2.333, 4.666, 8.888, 10.101}, false},
 		{"five increasing weights", []float64{1, 2, 3, 4, 5}, false},
+		{"thirty-two large number weights", getLargeWeights(32, 100000), false},
+		{"1k large number weights", getLargeWeights(1024, 1000000), false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -45,7 +48,7 @@ func TestWeightedChoice(t *testing.T) {
 				return
 			}
 
-			if !tt.wantErr {
+			if !tt.wantErr && len(tt.weights) <= 32 {
 				if !(0 <= gotIdx && gotIdx < len(tt.weights)) {
 					t.Errorf("WeightedChoice() got invalid index = %v, want = [0, %v)", gotIdx, len(tt.weights))
 					return
@@ -86,12 +89,12 @@ func TestWeightedShuffle(t *testing.T) {
 		{"only zero weights", []float64{0, 0}, true},
 		{"only non-positive weights", []float64{0, 0, -1}, true},
 		{"contains non-positive weights", []float64{-1, 10, 0}, true},
-		{"contains extremely larger weight 1", []float64{1e-6, 1e-1, 2e-2, 1e-3, 1e30}, true},
-		{"contains extremely larger weight 2", []float64{1e30, 1e-6}, true},
-		{"contains extremely larger weight 3", []float64{1e30, 1e30, 1e-6}, true},
-		{"contains extremely larger weight 4", []float64{1e30, 1e-6, 1e30}, true},
-		{"contains extremely larger weight 5", []float64{1e-6, 1e30, 1e30}, true},
-		{"contains extremely larger weight 6", []float64{1e-6, 1e30, 1e-3, 1}, true},
+		{"contains extremely larger weight 1", []float64{1e-6, 1e-1, 2e-2, 1e-3, 1e30}, false},
+		{"contains extremely larger weight 2", []float64{1e30, 1e-6}, false},
+		{"contains extremely larger weight 3", []float64{1e30, 1e30, 1e-6}, false},
+		{"contains extremely larger weight 4", []float64{1e30, 1e-6, 1e30}, false},
+		{"contains extremely larger weight 5", []float64{1e-6, 1e30, 1e30}, false},
+		{"contains extremely larger weight 6", []float64{1e-6, 1e30, 1e-3, 1}, false},
 		{"single weight", []float64{1}, false},
 		{"two diff weights", []float64{1, 3}, false},
 		{"two equal weights", []float64{2, 2}, false},
@@ -103,7 +106,9 @@ func TestWeightedShuffle(t *testing.T) {
 		{"three increasing weights", []float64{1, 100, 1000}, false},
 		{"four increasing weights", []float64{2.333, 4.666, 8.888, 10.101}, false},
 		{"five increasing weights", []float64{1, 2, 3, 4, 5}, false},
-		{"six repeated weights", []float64{1, 2, 1, 2, 1, 2}, false},
+		{"eight repeated weights", []float64{1, 2, 1, 2, 1, 2, 1, 2}, false},
+		{"thirty-two large number weights", getLargeWeights(32, 100000), false},
+		{"1k large number weights", getLargeWeights(1024, 1000000), false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -111,16 +116,26 @@ func TestWeightedShuffle(t *testing.T) {
 				t.Skipf("skipping large case '%v' in short mode", tt.name)
 			}
 
-			if err := WeightedShuffle(tt.weights, func(idx int) (err error) {
+			cnt, maxCnt := 0, len(tt.weights)
+			switch err := WeightedShuffle(tt.weights, func(idx int) (err error) {
+				cnt++
+				if (idx < 0) || (idx >= maxCnt) {
+					t.Errorf("WeightedShuffle() got invalid index = %v, want = [0, %v)", idx, maxCnt)
+				}
 				return
-			}); (err != nil) != tt.wantErr {
+			}); {
+			case (err != nil) != tt.wantErr:
 				t.Errorf("WeightedShuffle() got error = %v, wantErr = %v", err, tt.wantErr)
 				return
-			} else if (err != nil) && (err != errInvalidWeights) {
+			case (err != nil) && (err != errInvalidWeights):
 				t.Errorf("WeightedShuffle() got diff error = %v, want = %v, weights = %v", err, errInvalidWeights, tt.weights)
+				return
+			case (err == nil) && (cnt != maxCnt):
+				t.Errorf("WeightedShuffle() got not enough indexes = %v, want = %v", cnt, maxCnt)
+				return
 			}
 
-			if !tt.wantErr {
+			if !tt.wantErr && len(tt.weights) <= 32 {
 				checkProbDist(t, "WeightedShuffle", times, tt.weights, func() (idx int, err error) {
 					savedIdx, cnt := 0, 0
 					err = WeightedShuffle(tt.weights, func(idx int) error {
@@ -180,7 +195,7 @@ func BenchmarkWeightedShuffleInvalid(b *testing.B) {
 }
 
 func BenchmarkWeightedShuffleValid(b *testing.B) {
-	weights := []float64{2.333, 4.666, 8.888, 10.101}
+	weights := []float64{2.333, 4.666, 8.888, 10.101, 12.3333}
 	noop := func(idx int) (err error) { return }
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
@@ -188,10 +203,21 @@ func BenchmarkWeightedShuffleValid(b *testing.B) {
 	}
 }
 
+func getLargeWeights(count, scale int) (weights []float64) {
+	for i := 1; i <= count; i++ {
+		num := 99 + math.Pi*float64(scale)*math.Log2(float64(i+1))
+		if i%4 == 0 {
+			num = math.Log10(num)
+		}
+		weights = append(weights, num)
+	}
+	return
+}
+
 func checkProbDist(t *testing.T, name string, times int, weights []float64, idxFunc func() (idx int, err error)) {
 	var (
-		tolerance        = 0.16
-		minExpectedTimes = 10.0
+		tolerance        = 0.2
+		minExpectedTimes = 20.0
 	)
 
 	result := make(map[int]int)
@@ -221,10 +247,10 @@ func checkProbDist(t *testing.T, name string, times int, weights []float64, idxF
 	}
 
 	for i, w := range weights {
-		if (w <= 0) || ((w / weightSum * float64(times)) < minExpectedTimes) {
+		expected := w / weightSum
+		if (w <= 0) || ((expected * float64(times)) < minExpectedTimes) {
 			continue
 		}
-		expected := w / weightSum
 		actual := float64(result[i]) / float64(times)
 		if !isFloatEqual(actual, expected, tolerance) {
 			t.Errorf("%s() got unexpected result, weights: %v, index:[%d](%.2f), expected: %.3f, actual: %.3f, tole: %.2f%%",
